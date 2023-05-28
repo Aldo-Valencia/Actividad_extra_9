e51e5229b2ab394238fc34ba4876f06da41d4e6b
Lenovo <avalenciasandoval90@gmail.com>
2023-05-27 19:40:29 -0600

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+import org.example.controlador.ControladorLibro;
+import org.example.modelo.Libro;
+import org.example.persistencia.DemoLibroDB;
+import org.example.persistencia.LibroDAO;
+import org.example.vista.VentanaLibro;
+public class Main {
+    public static void main(String[] args) {
+        VentanaLibro view = new VentanaLibro("MVC y JDBC");
+        ControladorLibro controller = new ControladorLibro(view);
+
+
+        // LibroDAO ldao = new LibroDAO();
+
+
+
+
+
+
+
+
+
+        /*try {
+           Libro res = (Libro) ldao.buscarPorId("1");
+            System.out.println(res);
+            System.out.println("-----------------------------------");
+            for (Object lib: ldao.obtenerTodo()){
+                System.out.println((Libro)lib);
+            }
+        }catch (SQLException sqle){
+            System.out.println("Error ");
+            System.out.println(sqle.getMessage());
+        }*/
+
+
+
+    }
+
+
+    }
